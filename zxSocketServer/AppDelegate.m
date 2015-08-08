805348b60519fa36823a7950286f43a2a021720c
rayshen <rayshen1012@gmail.com>
2015-08-08 11:38:42 +0800

    1

--- zxSocketServer/AppDelegate.m
+++ zxSocketServer/AppDelegate.m
@@ -10,11 +10,56 @@
 
 @implementation AppDelegate
 
-@synthesize window = _window;
 
+@synthesize status;
+@synthesize port;
+@synthesize host;
+@synthesize window = _window;
+@synthesize socket;
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    port.stringValue = @"54321";
+}
+-(void)addText:(NSString *)str
+{
+    status.string = [status.string stringByAppendingFormat:@"%@\n",str];
+}
+- (IBAction)listen:(id)sender {
+    NSLog(@"listen");
+    
+    socket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
+    NSError *err = nil; 
+    if(![socket acceptOnPort:[port integerValue] error:&err]) 
+    { 
+        [self addText:err.description];
+    }else
+    {
+        [self addText:[NSString stringWithFormat:@"开始监听%d端口.",port.integerValue]];
+    }
 }
 
+- (void)socket:(GCDAsyncSocket *)sender didAcceptNewSocket:(GCDAsyncSocket *)newSocket
+{
+    // The "sender" parameter is the listenSocket we created.
+    // The "newSocket" is a new instance of GCDAsyncSocket.
+    // It represents the accepted incoming client connection.
+    
+    // Do server stuff with newSocket...
+    [self addText:[NSString stringWithFormat:@"建立与%@的连接",newSocket.connectedHost]];
+    
+    s = newSocket;
+    s.delegate = self;
+    [s readDataWithTimeout:-1 tag:0];
+}
+
+-(void)socket:(GCDAsyncSocket *)sock didReadData:(NSData *)data withTag:(long)tag
+{
+    NSString *receive = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    [self addText:[NSString stringWithFormat:@"%@:%@",sock.connectedHost,receive]];
+    
+    NSString *reply = [NSString stringWithFormat:@"收到:%@",receive];
+    
+    [s writeData:[reply dataUsingEncoding:NSUTF8StringEncoding] withTimeout:-1 tag:0];
+    [s readDataWithTimeout:-1 tag:0];
+}
 @end
